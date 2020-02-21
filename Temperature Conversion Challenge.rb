bec8c40e8b0a6476ee8134854ee4a0c9cd22f4c3
Reba Pipes <51538500+rebapipes@users.noreply.github.com>
2020-02-21 17:10:32 -0600

    Temperature Conversion Challenge
    Temperature Conversion Challenge (Ruby)

---
+++ Temperature Conversion Challenge.rb
@@ -0,0 +1,8 @@
+puts "Enter degrees in Celsius:"
+celsius = gets.chomp
+
+def celsius_to_fahrenheit(c)
+  fahrenheit = (c.to_f * 9/5) + 32
+end
+
+puts The temperature is {celsius_to_fahrenheit(celsius)} Farenheit
